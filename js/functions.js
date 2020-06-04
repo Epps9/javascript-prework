5d51ce9e2164244f6e0d1cee1cd0fbb79aca2720
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-06-04 11:11:33 +0200

    Add game result script

--- js/functions.js
+++ js/functions.js
@@ -38,3 +38,25 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień'&& playerMove == 'papier') {
+	printMessage('Ty wygrywasz!')
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+	printMessage('Ups, tym razem komputer wygrywa!')
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+	printMessage('Remis!')
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+	printMessage('Ups, tym razem komputer wygrywa!')
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+	printMessage('Ty wygrywasz!')
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+	printMessage('Remis!')
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+	printMessage('Ups, tym razem komputer wygrywa!')
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+	printMessage('Ty wygrywasz!')
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+	printMessage('Remis!')
+} else (playerMove == 'nieznany ruch') {
+	printMessage(Możesz wpisać jednynie liczny 1, 2 lub 3)
+}
