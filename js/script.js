c3aeb6dc6258d21e66aa731cbad205437d2f0469
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-06-07 15:20:41 +0200

    Add complete javascript

--- js/script.js
+++ js/script.js
@@ -1,78 +1,73 @@
+function getMoveName(argMoveId){
+	if(argMoveId == 1){
+	  return 'kamień';
+	} else if(argMoveId == 2) {
+		return 'papier';
+	} else if(argMoveId == 3) {
+		return 'nożyce';
+	}
+  
+	printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+	return 'nieznany ruch';
+  }
+function displayResult(computerMove, playerMove) {
+	console.log ('co się stało')
+	if(computerMove == 'kamień' && playerMove == 'papier'){
+		printMessage('Ty wygrywasz!');
+		} else if(computerMove == 'kamień' && playerMove == 'nożyce') {
+			printMessage('Przegrywasz!');
+		} else if(computerMove == 'kamień' && playerMove == 'kamień') {
+			printMessage('Remis!');
+		} else if(computerMove == 'papier' && playerMove == 'nożyce') {
+			printMessage('Ty wygrywasz!');
+		} else if(computerMove == 'papier' && playerMove == 'kamień') {
+			printMessage('Przegrywasz!');
+		} else if(computerMove == 'papier' && playerMove == 'papier') {
+			printMessage('Remis!');
+		} else if(computerMove == 'nożyce' && playerMove == 'kamień') {
+			printMessage('Ty wygrywasz!');
+		} else if(computerMove == 'nożyce' && playerMove == 'papier') {
+			printMessage('Przegrywasz!');
+		} else if(computerMove == 'nożyce' && playerMove == 'nożyce') {
+			printMessage('Remis!');
+		}
+}
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
+
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = getMove(randomNumber);
+let computerMove = getMoveName(randomNumber);
 
 /*
 if(randomNumber == 1){
   computerMove = 'kamień';
-} else if (randomNumber == 2) {
+} else if(randomNumber == 2) {
 	computerMove = 'papier';
 } else if (randomNumber == 3) {
 	computerMove = 'nożyce';
 }
 */
-
 printMessage('Mój ruch to: ' + computerMove);
+
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMove(randomNumber);
-
-let getMoveName = randomNumber;
-
-if(getMoveName == 2) {
-	getMoveName = 'papier'; 
-} else if (getMoveName == 3) {
-	getMoveName = 'nożyce';
-}
+let playerMove = getMoveName(playerInput);
 
 /*
 if(playerInput == '1'){
-  playerMove = 'kamień';
-} else if (randomNumber == 2) {
-	computerMove = 'papier';
-} else if (randomNumber == 3) {
-	computerMove = 'nożyce';
+  	playerMove = 'kamień';
+} else if(playerInput == 2) {
+	playerMove = 'papier';
+} else if (playerInput == 3) {
+	playerMove = 'nożyce';
+} else {
+	playerMove = 'nieznany ruch'
 }
 */
-
 printMessage('Twój ruch to: ' + playerMove);
 
-
-
-function getMove(argMoveId){
-	if(argMoveId == 1){
-	  return 'kamień';
-	} else {
-	  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-	  return 'nieznany ruch';
-	}
-  }
-
-function displayResult(argComputerMove, argPlayerMove) {
-	if (argComputerMove == 'kamień'&& argPlayerMove == 'papier') {
-		printMessage('Ty wygrywasz!')
-	} else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce') {
-		printMessage('Ups, tym razem komputer wygrywa!')
-	} else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień') {
-		printMessage('Remis!')
-	} else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-		printMessage('Ups, tym razem komputer wygrywa!')
-	} else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
-		printMessage('Ty wygrywasz!')
-	} else if (argComputerMove == 'papier' && argPlayerMove == 'papier') {
-		printMessage('Remis!')
-	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-		printMessage('Ups, tym razem komputer wygrywa!')
-	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-		printMessage('Ty wygrywasz!')
-	} else if (argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') {
-		printMessage('Remis!')
-	} else if (argPlayerMove == 'nieznany ruch') {
-        printMessage('Możesz wpisać jednynie liczny 1, 2 lub 3');
-    }
-}
-displayResult (argComputerMove, argComputerMove);
+displayResult(computerMove, playerMove) ;
